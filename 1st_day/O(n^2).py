a6b8ae8dbdeb15a69476aa0fd972df19568989d7
MAR miju <mar.miju.dev@gmail.com>
2025-01-30 22:37:50 +0600

    O(n^2)

---
+++ 1st_day/O(n^2).py
@@ -0,0 +1,16 @@
+
+
+"""
+Author: Azizar Rahman
+Position: Software Developer at Inovate Digital
+Date: 2025-01-30 21:27:48
+topic: O(n)
+desc:
+"""
+
+def Print_item(n) :
+    for i in range(n):
+        for j in range(n):
+           print(i,j)   
+
+Print_item(10) #O(n^2)
